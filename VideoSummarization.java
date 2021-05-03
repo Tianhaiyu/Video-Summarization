b77200c5a637ab192bbb7357bf9d4ccd6ba23c57
Ben Zhu <benzhu611@gmail.com>
2021-05-02 20:29:17 -0700

    Audio is now stored in Array list. Can play starting and ending at any frame. Added getters for some PlaySound stuff.

--- VideoSummarization.java
+++ VideoSummarization.java
@@ -1,7 +1,5 @@
 
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
@@ -29,8 +27,15 @@ public class VideoSummarization {
 	ArrayList<BufferedImage> FramesArray;
 	JFrame frame;
 	JLabel label;
+	//params use for break shots
+	private int threshold = 25;
+	private int sec_buffer = 1;
+
+	private int[][][] histogramPrev = new int[4][4][4];
+	private int[][][] histogramNext = new int[4][4][4];
+	ArrayList<Integer>  shots = new ArrayList<Integer>();
 	
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	
 	// get the command line parameters
 	if (args.length < 1) {
@@ -40,8 +45,11 @@ public class VideoSummarization {
 	VideoSummarization vs = new VideoSummarization();
 
 	vs.PlayInSync(args[0], args[1]);
+	
+	//vs.BreakInShots(args[1]);
 
-}
+	}
+
 	public void PlayInSync(String pathToWav, String pathToFrames){
 
 		// opens the inputStream
@@ -129,9 +137,14 @@ public class VideoSummarization {
 		Timer framesTimer = new Timer();
 
 		//small delay to make sure they start in sync
-		long startTime = System.currentTimeMillis() + 300;
+		long startTime = System.currentTimeMillis() + 1000;
 
-		Thread wavThread = new Thread(new PlaySound(wavInput));
+		//START AND END FRAME
+		int startFrame = 0;
+		int endFrame = 16200;
+
+
+		Thread wavThread = new Thread(new PlaySound(wavInput, startFrame, endFrame));
 
 		// initializes new thread to play sound
 		wavTimer.schedule(new TimerTask(){
@@ -181,5 +194,116 @@ public class VideoSummarization {
 			}
 		}, new Date(startTime + 666), 100);
 
+	}
+
+	public void BreakInShots(String pathToFrames) throws IOException{ 
+		BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"));
+		ReadBytes(pathToFrames, histogramPrev, 0);
+		//shots.add(0);
+		
+		int frameNumber = 2;
+	    int frameCounter = 0;
+	    int pastBreak = fps*sec_buffer;
+
+	    for(int i = 1; i < 30*60*9; i ++){
+	    	
+	    	if(frameCounter == frameNumber){
+	    		ClearHistogramNext();
+	    		ReadBytes(pathToFrames, histogramNext, i);
+	    		double val = SDvalue();
+	    		val = val / (width*height);
+	    		val *= 100;
+	    		
+	    		//String s1 = "val: " + val;
+	    		//String s2 = "frame: " + i; 
+				//writer.write(s1+"\n");
+				//writer.write(s2+"\n");
+				//writer.flush();
+				//if SDValue is greater than threshold
+				//add frame index, which is the start of new shot to the ds
+				if(val > threshold && pastBreak <= 0 && val < 200){
+					shots.add(i);
+					pastBreak = fps*sec_buffer;
+				}
+				CopyHistogramBack();
+				frameCounter = 0;
+			} else {
+				//do nothing, skip frames here
+			}
+			frameCounter++;
+			pastBreak--;
+		}
+		for(Integer i : shots){
+			writer.write("Frame:" + String.valueOf(i) + "\n");
+		}
+		writer.close();
+		System.out.println("Number of shots: " + shots.size());
+	}
+
+	public void ReadBytes(String pathToFrames, int[][][] histogram, int i){
+		try{
+			int frameLength = width*height*3;
+			String imgPath = pathToFrames + "frame" + i + ".rgb";
+			File file = new File(imgPath);
+			RandomAccessFile raf = new RandomAccessFile(file, "r");
+			raf.seek(0);
+
+			
+			long len = frameLength;
+			byte[] bytes = new byte[(int) len];
+
+			raf.read(bytes);
+
+			int ind = 0;
+			for(int y = 0; y < height; y++)
+			{
+				for(int x = 0; x < width; x++)
+				{
+					byte r = bytes[ind];
+					byte g = bytes[ind+height*width];
+					byte b = bytes[ind+height*width*2]; 
+					//extracts the two most siginificant bits 
+					int ri = (int)( (r & 0xff) &0xC0);
+					int gi = (int)( (g & 0xff) &0xC0);
+					int bi = (int)( (b & 0xff) &0xC0);
+					
+					histogram[ri/64][gi/64][bi/64]++;	
+					ind++;
+				}
+			}
+			raf.close();
+		}
+		catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		catch(IOException e){
+			e.printStackTrace();				
+		}
+	}
+	
+	private void CopyHistogramBack(){
+		for(int i = 0; i < 4; i++)
+			for(int j = 0; j < 4; j++)
+				for(int k = 0; k < 4; k++){
+					histogramPrev[i][j][k] = histogramNext[i][j][k]; 
+				}
+	}
+	
+	private void ClearHistogramNext(){
+		for(int i = 0; i < 4; i++)
+			for(int j = 0; j < 4; j++)
+				for(int k = 0; k < 4; k++){
+					histogramNext[i][j][k] = 0; 
+				}
+	}
+	//measures change of color intensity between two frames
+	private double SDvalue(){
+		int sum = 0;
+		for(int i = 0; i < 4; i++)
+			for(int j = 0; j < 4; j++)
+				for(int k = 0; k < 4; k++){
+					sum += Math.abs(histogramPrev[i][j][k] - histogramNext[i][j][k]);
+				}
+		return ((double)sum);
 	}
 }
